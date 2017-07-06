72008ce49ce9857d5d4be5b3e0b623cb27bca9cf
Imagic <imagic@imagic.com>
2017-07-06 03:14:49 +0200

    Init.

---
+++ loupgg/gamemode/shared.lua
@@ -0,0 +1,9 @@
+DeriveGamemode("sandbox")
+
+GM.Name = "LoupGG"
+GM.Author = "Nadochima & ImagicTheCat"
+GM.Email = "N/A"
+GM.Website = "https://github.com/Nadochima/LoupGG"
+
+function GM:Initialize()
+end
