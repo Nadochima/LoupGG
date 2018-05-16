3fd0e871949e1ea06568898f5526ddb0c0c8d885
Nadochima <mine_prox@outlook.fr>
2018-05-16 10:57:48 +0200

    Add content file

--- loupgg_content/lua/autorun/sv_loupgg_materials.lua
+++ loupgg_content/lua/autorun/sv_loupgg_materials.lua
@@ -1,1 +1,17 @@
-resource.AddFile( "materials/loupgg/loupgg_ui01.png" )
+
+--UI
+resource.AddFile("materials/loupgg/loupgg_button.png")
+resource.AddFile("materials/loupgg/loupgg_frame.png")
+resource.AddFile("materials/loupgg/loupgg_logo.png")
+resource.AddFile("materials/loupgg/loupgg_ui01.png")
+resource.AddFile("materials/loupgg/loupgg_ui02.png")
+
+--Wooden wheels
+resource.AddFile("materials/loupgg/loupgg_woodwheel01.png")
+resource.AddFile("materials/loupgg/loupgg_woodwheel02.png")
+resource.AddFile("materials/loupgg/loupgg_woodwheel03.png")
+
+--Steampunk wheels
+resource.AddFile("materials/loupgg/loupgg_spbase.png")
+resource.AddFile("materials/loupgg/loupgg_spwheel1.png")
+resource.AddFile("materials/loupgg/loupgg_spwheel2.png")
